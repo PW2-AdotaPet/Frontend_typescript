7af0dcf87f0851263354cd926ac310fde303d948
Jonathaannn <jonathanjog237@gmail.com>
2023-11-05 13:24:04 -0300

    Inicando projeto react em com typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Testando</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni veritatis tempore beatae doloribus fuga aperiam possimus, commodi ipsam numquam eum illum obcaecati, vitae repudiandae aliquam deleniti praesentium, non earum atque.</p>
+      </div>
     </div>
   );
 }
