09b5eb8fe64560a66eca9cd61308e9cda9759029
ivesfg1 <wigzfurtado@gmail.com>
2023-12-11 21:40:26 -0300

    logout completo

--- src/Components/Pages/Account.tsx
+++ src/Components/Pages/Account.tsx
@@ -10,7 +10,13 @@ import Profile from "../Ui/Profile";
 import Input from "../Forms/Input";
 import NavBar from "../Layouts/NavBar";
 
+
+import { useAuth } from "../../Context/AuthContext";
+
 function Account() {
+
+  const { logout } = useAuth();
+
   const teste = {
     image:
       "https://s3-alpha-sig.figma.com/img/3eaf/9f72/591b5570913ed4517e6b6d622943cf98?Expires=1700438400&Signature=PX97k7VJJ~qL09-B78olq3UE6wnxLzLPR7DWpcQ9B6yx0CA7KSm-6a4U5DWsyx8Zge10N8C7~nbdDD-owROF5KJIR~hcWc3s9bkSRwgnpd6EU0HlsjcC8AU6wgZxZLCC0vql4-p~3UoMQv4Fm0ZDDj5fWE6RrKxbpYH1ShLJ6QyNZd~IVgSyXhRUKJ6D3~TVKE~XsQoNrmCuZqaup03CwKiNL~F3G2f2dAG8MpAQK0N6eODlJ28OedFpdKXT3bjO78wnfJ2WbXBo2ssY2ePrV2MbWg5uofoxpVQ4SQeNgBpTOj5~PoFp1BLnu4H9vDzxt-JNvBUDpTDnDAZIaWZ2FQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
@@ -73,7 +79,7 @@ function Account() {
         <DividerContainer>
           <Button name="Editar Conta" customClass="outline" />
           <Button name="Apagar Conta" customClass="outline" />
-          <Button name="Sair" customClass="danger" />
+          <Button name="Sair" customClass="danger" handle={logout} />
         </DividerContainer>
       </Container>
     </Container>
